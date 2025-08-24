78e636cd4e6c3f35e6b1db066f323baac65b4cb5
chaeeah <dncodk@naver.com>
2025-08-25 00:35:19 +0900

    Chore: 기본 프로젝트 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import MainPage from './pages/MainPage';
+import WritingPage from './pages/WritingPage';
+
 function App() {
   return (
-    <h1>
-      Hello World!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<LandingPage />} />
+        <Route path='main' element={<MainPage />} />
+        <Route path='write' element={<WritingPage />} />
+      </Routes>
+    </BrowserRouter>
     );
 }
 
